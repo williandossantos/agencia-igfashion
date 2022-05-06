d67a5c37c4b72be7f805a8f113225438b9a3f298
Willian dos Santos <82684893+williandossantos@users.noreply.github.com>
2022-05-06 13:13:38 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+let button = document.querySelector(".btn-menu")
+let navbar = document.querySelector(".navbar-top")
+button.addEventListener('click', ()=>{
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+      } else {
+        navbar.style.display = "block";
+      }
+})
+
